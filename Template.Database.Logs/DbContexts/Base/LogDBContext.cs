88b7f2a94871f0b7527fae6a7e0d9570040edf92
magals <itengineer@live.ru>
2022-12-03 13:19:34 +0300

    add: LogDBContext

---
+++ Template.Database.Logs/DbContexts/Base/LogDBContext.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Template.Database.Logs.DbContexts.Base
+{
+
+	public class LogDBContext : DbContext
+	{
+		public DbSet<LogMessageEntity>? LogMessageEntity { get; set; }
+
+		public LogDBContext(DbContextOptions<LogDBContext> options)
+				: base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+
+			modelBuilder.Entity<LogMessageEntity>(entity =>
+			{
+				entity.Property(e => e.LogLevel).HasConversion<string>();
+				entity.HasData(new LogMessageEntity
+				{
+					Id = 1,
+					LogLevel = LogLevel.Information,
+					DateTimeOffset = DateTimeOffset.UtcNow,
+					Message = "First Message",
+					ThreadId = Thread.CurrentThread.ManagedThreadId
+				});
+			});
+		}
+
+	}
+}
