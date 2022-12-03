ee6a60d69e973be914f8df92d0c9e05e57eeafad
magals <itengineer@live.ru>
2022-12-03 13:19:43 +0300

    add: LogMessageRepository

---
+++ Template.Database.Logs/Repositories/LogMessageRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace Template.Database.Logs.Repositories
+{
+	public class LogMessageRepository
+	{
+		private readonly LogDBContext _context;
+		public LogMessageRepository(LogDBContext context)
+		{
+			_context = context;
+		}
+
+		public void Create(LogMessageEntity customer)
+		{
+			ArgumentNullException.ThrowIfNull(_context.LogMessageEntity);
+		
+			_context.LogMessageEntity.Add(customer);
+			_context.SaveChanges();
+		}
+	}
+}
