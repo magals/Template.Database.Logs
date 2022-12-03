57cfd5ab2d9959a9a1f1be3b35347395d7a78c12
magals <itengineer@live.ru>
2022-12-03 13:18:13 +0300

    add: models Entity, LogMessageEntity

---
+++ Template.Database.Logs/DbContexts/Base/Models/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace Template.Database.Logs.DbContexts.Base.Models
+{
+	  public class Entity
+    {
+        [Key]
+        public long Id { get; set; }
+    }
+}
---
+++ Template.Database.Logs/DbContexts/Base/Models/LogMessageEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace Template.Database.Logs.DbContexts.Base.Models
+{
+	public class LogMessageEntity : Entity
+	{
+		[Required]
+		public DateTimeOffset DateTimeOffset { get; set; }
+
+		[Required]
+		public LogLevel LogLevel { get; set; }
+
+		[Required]
+		[StringLength(500)]
+		public string Message { get; set; } = string.Empty;
+
+		[Required]
+		public int ThreadId { get; set; }
+	}
+}
