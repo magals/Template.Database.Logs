fb437d2646836bfe4a78d20ccfc5320a869db30d
magals <itengineer@live.ru>
2022-12-03 13:18:59 +0300

    add: GlobalUsings

---
+++ Template.Database.Logs/GlobalUsings.cs
@@ -0,0 +1,21 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.ComponentModel;
+global using System.Globalization;
+global using System.ComponentModel.DataAnnotations;
+
+global using Microsoft.Extensions.Logging;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+global using Template.Database.Logs;
+global using Template.Database.Logs.DbContexts.Base.Models;
+global using Template.Database.Logs.Repositories;
+global using Template.Database.Logs.DbContexts.Base;
+
+
+
+
